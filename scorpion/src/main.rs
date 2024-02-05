f649eebdeb380ed30be3e506ab8a38f50f58f060
NewIron7 <hboissel@student.42.fr>
2024-02-05 16:38:57 +0100

    first version scorpion

--- scorpion/src/main.rs
+++ scorpion/src/main.rs
@@ -1,63 +1,75 @@
-use exif;
+use std::fs::File;
+use std::io::BufReader;
+use exif::Reader;
+use chrono::offset::Utc;
+use chrono::DateTime;
+use std::time::SystemTime;
 
 /// Get all arguments from the command line
 /// They are stored in a vector of strings
 /// They represent filnames
 fn main() {
     let args: Vec<String> = std::env::args().collect();
-    // remove the first argument which is the name of the program
     let args: Vec<String> = args[1..].to_vec();
+
+    if args.is_empty() {
+        println!("🦂 Scorpion 🦂");
+        println!("A simple command line tool to view metadata and EXIF data of files");
+        println!("Usage: scorpion <file1> <file2> ...");
+        return;
+    }
     
-    print_file_content(args);
+    for arg in args {
+        println!("🖼️  {}", arg);
+        print_metadata(&arg);
+        let r = print_exit_data(&arg);
+        if r.is_err() {
+            println!("🚫 No EXIF data found");
+        }
+        println!("\n{}\n", "─".repeat(50));
+    }
 }
 
-/// Function that loop through the filenames
-/// and print the EXIF data of the pictures
-/// It takes a vector of strings as argument
+
+/// Function to print EXIF data from a file
+/// It takes a path to a file as an argument
+/// It returnes nothing
+/// It prints the EXIF data of the file
+fn print_exit_data(file: &str) -> Result<(), ()> {
+    let file = File::open(file).expect("Failed to open file");
+    let mut reader = BufReader::new(&file);
+
+    let exif_reader = Reader::new();
+    let exif_data = exif_reader.read_from_container(&mut reader);
+    let exif_data = match exif_data {
+        Ok(exif_data) => exif_data,
+        Err(_) => return Err(()),
+    };
+
+    for field in exif_data.fields() {
+        println!("{}: {}", field.tag, field.display_value().with_unit(&exif_data));
+    }
+    Ok(())
+}
+
+/// Function that prints all the metadata of a file
+/// It takes a path to a file as an argument
 /// It returns nothing
-/// It uses the get_picture_content and get_exif_data functions
-/// to get the content of the files and the EXIF data
-/// and print the EXIF data
-fn print_file_content(args: Vec<String>) {
-    for filename in args {
-        let content = get_picture_content(&filename);
-        let exif_data = get_exif_data(content);
-        // print the filename with an emoji
-        println!("📷 {}", filename);
-        println!("{}", exif_data);
-    }
+fn print_metadata(file: &str) {
+    let metadata = std::fs::metadata(file).expect("Failed to read metadata");
+    println!("Size: {:.2} MB", metadata.len() as f64 / 1_000_000.0);
+    println!("Modified: {}", format_time(metadata.modified().unwrap()));
+    println!("Created: {:?}", format_time(metadata.created().unwrap()));
+    println!("Accessed: {:?}", format_time(metadata.accessed().unwrap()));
+    println!();
 }
 
-/// Function that get EXIF data in a vector of bytes
-/// It takes a vector of bytes as argument
-/// it returns the EXIF data as a string
-/// if a error occurs, it returns an empty string
-/// and print the error with a emoji
-/// It uses the kamadak-exif crate
-/// to parse the EXIF data
-fn get_exif_data(content: Vec<u8>) -> String {
-    match exif::parse_exif(&content) {
-        Ok(exif) => exif.to_string(),
-        Err(e) => {
-            println!("😭 {}", e);
-            String::new()
-        }
-    }
+/// It takes a SystemTime as an argument
+/// It returns a string
+/// It formats the time into a readable format
+/// It uses the chrono library
+fn format_time(time: SystemTime) -> String {
+    let datetime: DateTime<Utc> = time.into();
+    let time_readable = format!("{}", datetime.format("%d/%m/%Y %T"));
+    time_readable
 }
-
-/// Function that read the content of a file
-/// those files are pictures
-/// It takes a filename as argument
-/// It returns a vector of bytes
-/// if a error occurs, it returns an empty vector
-/// and print the error with a emoji
-fn get_picture_content(filename: &str) -> Vec<u8> {
-    match std::fs::read(filename) {
-        Ok(content) => content,
-        Err(e) => {
-            println!("{}:😭 {}", filename, e);
-            Vec::new()
-        }
-    }
-}
-
